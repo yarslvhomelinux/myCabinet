87b42bcf72e8120749469f172eb990208fb57ae9
lebedev <lebedev@haulmont.com>
2018-11-21 22:53:20 +0400

    Fixed user view, workflow upgraded

--- modules/web/src/com/company/mycabinet/web/request/CustomerRequestsBrowse.java
+++ modules/web/src/com/company/mycabinet/web/request/CustomerRequestsBrowse.java
@@ -1,6 +1,29 @@
 package com.company.mycabinet.web.request;
 
+import com.company.mycabinet.entity.Request;
+import com.company.mycabinet.entity.Response;
+import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.gui.WindowManager;
 import com.haulmont.cuba.gui.components.AbstractLookup;
+import com.haulmont.cuba.gui.components.GroupTable;
+import com.haulmont.cuba.gui.components.Table;
+
+import javax.inject.Inject;
 
 public class CustomerRequestsBrowse extends AbstractLookup {
+
+    @Inject
+    protected GroupTable<Request> requestsTable;
+    @Inject
+    protected Metadata metadata;
+
+    public void onCreateResponseClick() {
+        if (requestsTable.getSingleSelected() != null) {
+            Response response = metadata.create(Response.class);
+            response.setRequest(requestsTable.getSingleSelected());
+            openEditor(response, WindowManager.OpenType.NEW_TAB);
+        } else {
+            showNotification(getMessage("notSelectedRequestError"));
+        }
+    }
 }
--- modules/web/src/com/company/mycabinet/web/response/ResponseEdit.java
+++ modules/web/src/com/company/mycabinet/web/response/ResponseEdit.java
@@ -1,7 +1,56 @@
 package com.company.mycabinet.web.response;
 
+import com.company.mycabinet.entity.State;
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.gui.WindowManager;
 import com.haulmont.cuba.gui.components.AbstractEditor;
 import com.company.mycabinet.entity.Response;
+import com.haulmont.cuba.gui.components.GroupBoxLayout;
+
+import javax.inject.Inject;
+import java.util.List;
 
 public class ResponseEdit extends AbstractEditor<Response> {
+
+    @Inject
+    protected DataManager dataManager;
+    @Inject
+    protected GroupBoxLayout closeCommentGroupBox;
+
+    @Override
+    public void ready() {
+        super.ready();
+        if (getItem().getRequest() == null || State.CUSTOMER_FEEDBACK_RECEIVED.equals(getItem().getRequest().getStatus())) {
+            closeCommentGroupBox.setEnabled(true);
+        }
+    }
+
+    public void onOpenRequestEditorButtonClick() {
+        if (getItem().getRequest() != null) {
+            openEditor(getItem().getRequest(), WindowManager.OpenType.NEW_TAB);
+        } else {
+            showNotification(getMessage("requestIsNull"));
+        }
+    }
+
+    public void onOpenRequestBrowserButtonClick() {
+        openWindow("mycabinet$CustomerRequests.browse", WindowManager.OpenType.NEW_TAB);
+    }
+
+    @Override
+    protected boolean postCommit(boolean committed, boolean close) {
+        if (super.postCommit(committed, close)) {
+            if (getItem().getRequest() != null) {
+                getItem().getRequest().setStatus(State.RESPONSE_RECEIVED);
+                List<Response> responseList = getItem().getRequest().getResponse();
+                responseList.add(getItem());
+                getItem().getRequest().setResponse(responseList);
+                dataManager.commit(getItem().getRequest());
+            }
+
+            return true;
+        }
+
+        return false;
+    }
 }
